21264fad890c53adfa5a19063baf96363fa23f7b
David <david.garcia@lumation.co>
2021-02-27 11:43:45 -0600

    Se hicieron cambios de rutas y se agrego bodyParser para middleware

--- app.js
+++ app.js
@@ -2,7 +2,25 @@
 
 //Variables Globales
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require("cors");
 const app = express();
 
 //Exportar 
+module.exports = app;
+
+//Rutas
+const user_routes = require('./src/routes/users.routes');
+
+//Middlewares
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//Cabeceras
+app.use(cors());
+
+//Carga de rutas
+app.use('/api', user_routes);
+
+//Exportación
 module.exports = app;
