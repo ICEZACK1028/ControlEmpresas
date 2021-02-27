1034871b5080e2b32ad4552be9c118a1c3acd4f1
David <david.garcia@lumation.co>
2021-02-27 15:23:41 -0600

    Se actualizaron las rutas

--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -10,7 +10,7 @@ var md_authentication = require('../middlewares/authenndicated');
 //Rutas
 var api = express.Router();
 api.post('/createEmployee', md_authentication.ensureAuth, employeeController.createEmployee);
-api.put('/editEmployee/:idEmployee', md_authentication.ensureAuth, employeeController.editEmployee);
-api.delete('/deleteEmployee/:idEmployee', md_authentication.ensureAuth, employeeController.deleteEmployee);
+api.put('/editEmployee/:idEmployee/:idCompany', md_authentication.ensureAuth, employeeController.editEmployee);
+api.delete('/deleteEmployee/:idEmployee/:idCompany', md_authentication.ensureAuth, employeeController.deleteEmployee);
 
 module.exports = api;
