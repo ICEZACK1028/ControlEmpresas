09a27a113d2e3551377e5334bb83e33e3736af8d
David <david.garcia@lumation.co>
2021-02-27 13:12:05 -0600

    se han agregar las rutas de crear y editar empleado

--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -1,1 +1,15 @@
 'use strict'
+
+//Importaciones
+const express = require ('express');
+const employeeController = require('../controllers/employee.controller');
+
+//middleware
+var md_authentication = require('../middlewares/authenndicated');
+
+//Rutas
+var api = express.Router();
+api.post('/createEmployee', md_authentication.ensureAuth, employeeController.createEmployee);
+api.put('/editEmployee/:idEmployee', md_authentication.ensureAuth, employeeController.editEmployee);
+
+module.exports = api;
