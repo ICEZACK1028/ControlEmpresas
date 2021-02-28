e94d2a9608029c8599e054fbb457b0b765b48a99
David <david.garcia@lumation.co>
2021-02-28 12:52:25 -0600

    Se agregaron rutas

--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -10,7 +10,12 @@ var md_authentication = require('../middlewares/authenndicated');
 //Rutas
 var api = express.Router();
 api.post('/createEmployee', md_authentication.ensureAuth, employeeController.createEmployee);
-api.put('/editEmployee/:idEmployee/:idCompany', md_authentication.ensureAuth, employeeController.editEmployee);
-api.delete('/deleteEmployee/:idEmployee/:idCompany', md_authentication.ensureAuth, employeeController.deleteEmployee);
+api.put('/editEmployee/:idEmployee', md_authentication.ensureAuth, employeeController.editEmployee);
+api.delete('/deleteEmployee/:idEmployee', md_authentication.ensureAuth, employeeController.deleteEmployee);
+api.get('/getEmployeeID/:idEmployee', md_authentication.ensureAuth, employeeController.searchEmployeeID);
+api.get('/getEmployeeName/:nameEmployee', md_authentication.ensureAuth, employeeController.searchEmployeeName);
+api.get('/getEmployeeDepartment/:depEmployee', md_authentication.ensureAuth, employeeController.searchEmployeeDepartment);
+api.get('/getEmployeeAll', md_authentication.ensureAuth, employeeController.searchEmployeeAll);
+api.get('/createEmployeePDF',md_authentication.ensureAuth, employeeController.createEmployeePDF);
 
 module.exports = api;
